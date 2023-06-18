64a22dafdcac08043a96fdc482c32bb877104a80
Vishwanath-Ramesh <vishwanathr.dev@outlook.com>
2023-06-18 07:46:39 +0200

    React and Webpack upgrade

--- root/client/jest.config.js
+++ root/client/jest.config.js
@@ -2,7 +2,8 @@ module.exports = {
   testEnvironment: 'jest-environment-jsdom', // Setting the test environment to node. i.e., window/DOM object is not accessible
   moduleDirectories: ['node_modules'],
   moduleNameMapper: {
-    '\\.css$': require.resolve('./test/style-mock.js'), // If you are importing the React component which in turn imports css files internally, jest throws Sytax error. For that, we need resolve to mock module.
+    '\\.css$': require.resolve('./test/mockModule.js'), // If you are importing the React component which in turn imports css files internally, jest throws Syntax error. For that, we need resolve to mock module.
+    'assets/(.*)': require.resolve('./test/mockModule.js'), // Mock for assets
   },
   setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'], // Imports the supplied files in all the test suites
   collectCoverageFrom: ['**/src/**/*.js'], // To cover all the files inside the src folder while generating the test report
